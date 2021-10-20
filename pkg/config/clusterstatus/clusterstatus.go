946f490e1e548edfd014204c8b2a1ec3c2e00257
egli <ehila@redhat.com>
2021-10-20 15:51:27 -0400

    feat: added helper pkg to get cluster status
    added helper methods to fetch cluster status
    
    Signed-off-by: egli <ehila@redhat.com>

---
+++ pkg/config/clusterstatus/clusterstatus.go
@@ -0,0 +1,31 @@
+package clusterstatus
+
+import (
+	"context"
+
+	configv1 "github.com/openshift/api/config/v1"
+	openshiftcorev1 "github.com/openshift/client-go/config/clientset/versioned/typed/config/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"k8s.io/client-go/rest"
+)
+
+func GetClusterStatus(ctx context.Context, restClient *rest.Config) (*configv1.InfrastructureStatus, error) {
+	client, err := openshiftcorev1.NewForConfig(restClient)
+	if err != nil {
+		return nil, err
+	}
+	infra, err := client.Infrastructures().Get(ctx, "cluster", metav1.GetOptions{})
+	if err != nil {
+		return nil, err
+	}
+	return &infra.Status, nil
+}
+
+func GetClusterStatusOrDie(ctx context.Context, restClient *rest.Config) *configv1.InfrastructureStatus {
+	infra, err := GetClusterStatus(ctx, restClient)
+	if err != nil {
+		panic(err)
+	}
+	return infra
+}
